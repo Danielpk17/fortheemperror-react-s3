f78b20d5acc83ce859ffa21e1ae6a59646a1c1b1
Emperror <danieldalpiva445@gmail.com>
2026-05-12 22:22:22 -0300

    feat: Lista de Tarefas6

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,53 @@
 import { useState } from 'react'
-import FormularioCadastro from "./FormularioCadastro"
-import CartaoTecnologia from "./CartaoTecnologia"
-import Contador2 from "./Contador2"
+import ListaTarefas6 from './ListaTarefas6'
+// import FormularioCadastro from "./FormularioCadastro"
+// import CartaoTecnologia from "./CartaoTecnologia"
+// import Contador2 from "./Contador2"
 
 
 function App() {
-  const [mostrar, setMostrar] = useState(true)
-  const [tecnologias, setTecnologias] = useState([])
 
-   function Adicionar(novaTecnologia) {
-        setTecnologias([...tecnologias, novaTecnologia])
-      }
-
-    function remove(id) {
-        setTecnologias(tecnologias.filter((t) => t.id !== id))
-    }
-
-  return (
-
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-
-  <FormularioCadastro aoAdicionar={Adicionar}/>
-  <Contador2 total={tecnologias.length} />
-  <button onClick={() => setMostrar(!mostrar)}>
-    {mostrar ? "Esconder" : "Mostrar"}
-  </button>
-
-    {!mostrar ? (
-    <p>Lista oculta</p>
-    ) : tecnologias.length === 0 ? (
-      <p>Nenhuma Tecnologia</p>
-    ) : (
-      <ul>
-        {tecnologias.map((t) => (
-          <CartaoTecnologia key={t.id} nome={t.nome} nivel={t.nivel} aoRemover={() => remove(t.id)} />
-        ))}
-
-      </ul>    
-)}
-
+   return (
+    <div>
+      <ListaTarefas6 />
     </div>
   )
+//   const [mostrar, setMostrar] = useState(true)
+//   const [tecnologias, setTecnologias] = useState([])
+
+//    function Adicionar(novaTecnologia) {
+//         setTecnologias([...tecnologias, novaTecnologia])
+//       }
+
+//     function remove(id) {
+//         setTecnologias(tecnologias.filter((t) => t.id !== id))
+//     }
+
+//   return (
+
+//     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+
+//   <FormularioCadastro aoAdicionar={Adicionar}/>
+//   <Contador2 total={tecnologias.length} />
+//   <button onClick={() => setMostrar(!mostrar)}>
+//     {mostrar ? "Esconder" : "Mostrar"}
+//   </button>
+
+//     {!mostrar ? (
+//     <p>Lista oculta</p>
+//     ) : tecnologias.length === 0 ? (
+//       <p>Nenhuma Tecnologia</p>
+//     ) : (
+//       <ul>
+//         {tecnologias.map((t) => (
+//           <CartaoTecnologia key={t.id} nome={t.nome} nivel={t.nivel} aoRemover={() => remove(t.id)} />
+//         ))}
+
+//       </ul>    
+// )}
+
+//     </div>
+//   )
 }
 
 export default App
---
+++ src/ListaTarefas6.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+function ListaTarefas6 () {
+
+    const [text, setText] = useState("");
+    const [list, setList] = useState([]);
+
+    function Adicionar () {
+        const Item = {
+            id: Date.now(),
+            nome: text
+        }
+        setList([... list,Item])
+        setText("")
+    }
+
+    function Remove (id) {
+        setList(list.filter((t) => t.id !== id))
+    }
+
+    return (
+        <div>
+        <input value={text} onChange={(e) => setText(e.target.value)} placeholder="Digite um Texto"/>
+        <button onClick={Adicionar}>Adicionar</button>
+        {list.length === 0 ? (
+            <p>Nenhum item na lista</p>
+        ) : ( <ul>
+            {list.map((t) => (
+                <li key={t.id}>
+                    {t.nome}
+                    <button onClick={() => Remove(t.id)}>Remover</button>
+                </li>
+            ))}
+        </ul>
+            
+        )}
+       
+        </div>
+    )
+}
+
+export default ListaTarefas6;
